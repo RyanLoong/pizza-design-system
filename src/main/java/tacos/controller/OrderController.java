2528bfe5cd789d4128c461003224cd5c2d14f1ed
ryanloong <412579870@qq.com>
2021-05-09 00:10:38 +0800

    完成用户注册、登录验证、披萨设计、订单提交功能

--- src/main/java/tacos/controller/OrderController.java
+++ src/main/java/tacos/controller/OrderController.java
@@ -3,6 +3,9 @@ package tacos.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;;
 import org.springframework.web.bind.support.SessionStatus;
+import tacos.config.OrderProps;
 import tacos.entity.Order;
 import tacos.entity.User;
 import tacos.repository.OrderRepository;
@@ -33,11 +37,14 @@ public class OrderController {
 
     private static final Logger logger = LoggerFactory.getLogger(OrderController.class);
 
+
     private OrderRepository orderRepo;
+    private OrderProps props;
 
     @Autowired
-    public OrderController(OrderRepository orderRepo) {
+    public OrderController(OrderRepository orderRepo, OrderProps props) {
         this.orderRepo = orderRepo;
+        this.props = props;
     }
 
     /**
@@ -51,6 +58,7 @@ public class OrderController {
 
     /**
      * 处理页面提交过来的包含order对象的表单信息。
+     * @AuthenticationPrincipal 可以获取到user上下文对象，其实就是相当于我们使用threadLocal存了一下user
      */
     @PostMapping
     public String processOrder(@Valid Order order, Errors errors,
@@ -66,5 +74,13 @@ public class OrderController {
         logger.info("Order submitted: " + order);
 
         return "redirect:/";
+    }
+
+    @GetMapping
+    public String ordersForUser(@AuthenticationPrincipal User user, Model model) {
+        Pageable pageable = PageRequest.of(0,props.getPageSize());
+        model.addAttribute("orders", orderRepo.findByUserOrderByPlacedAtDesc(user, pageable));
+        // 渲染订单页面
+        return "orderList";
     }
 }
--- src/main/java/tacos/repository/OrderRepository.java
+++ src/main/java/tacos/repository/OrderRepository.java
@@ -1,7 +1,9 @@
 package tacos.repository;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import tacos.entity.Order;
+import tacos.entity.User;
 
 import java.util.Date;
 import java.util.List;
@@ -16,7 +18,7 @@ public interface OrderRepository extends CrudRepository<Order, Long> {
     /**
      * 通过右边查询使用指定邮编的订购者
      * @param deliveryZip 邮编
-     * @return 返回一个订购者集合
+     * @return 返回一个订购集合
      */
     List<Order> findByDeliveryZip(String deliveryZip);
 
@@ -25,7 +27,14 @@ public interface OrderRepository extends CrudRepository<Order, Long> {
      * @param deliveryZip 邮编
      * @param startData 开始时间
      * @param endDate 结束时间
-     * @return 订购者集合
+     * @return 订单集合
      */
     List<Order> readOrdersByDeliveryZipAndPlacedAtBetween(String deliveryZip, Date startData, Date endDate);
+
+    /**
+     * 通过user对象去查询订单并降序显示
+     * @param user user对象
+     * @return 返回一个订单集合
+     */
+    List<Order> findByUserOrderByPlacedAtDesc(User user, Pageable pageable);
 }
